68e9ff8a95401b78929db43fa91920569b214b89
anonsoonye2001 <anonsoonye2001@yahoo.co.uk>
2019-09-30 15:40:03 -0500

    edit

--- Classwork-09-30-2019.R
+++ Classwork-09-30-2019.R
@@ -1,5 +1,6 @@
 load("fish_data.Rdata")
-F<-Fish
+
+f<-fish
  # subsetting---
 # indexing by condition
 fd<- f[f$depth_fac=="Deep",]
@@ -11,7 +12,7 @@ fd2<-subset(x=f,depth_fac=="Deep")
 fd4<-subset(x=f,depth_fac=="Deep", select=c("transect.id","area_fac"))
 ##filter
 library(dplyr)
-fd3<-filter(.datra=f, depth_fac=="Deep")
+fd3<-filter(.data=f, depth_fac=="Deep")
 
 ##which
 fd5<-f[which(f$depth_fac=='Deep' & f$area_fac=="east"),]
@@ -33,4 +34,28 @@ c1<-subset(x=f,depth_fac=="Deep", select=c("transect.id","area_fac"))
 c2<-subset(x=f,depth_fac=="Deep", select=c("area_fac","parcel.length.m","group"))
 c3<-cbind(c1,c2)
 head(c3)
-##filter
+
+
+#merging 2 dataframes ensuring that observations from one dataframe are connected with
+#observationin the second data frame correctly
+m1<-subset(x=f,depth_fac=="Deep", select=c("transect.id","area_fac"))
+m1$seq<-seq(1,nrow(m1),1)
+head(m1)
+m2<-subset(x=f,depth_fac=="Deep", select=c("transect.id","area_fac","parcel.length.m","group"))
+m2$seq<-seq(1,nrow(m2),1)
+head(m2)
+
+# create a sequence of data
+m2$seq<-seq(from=1, to=nrow(m2),by=1)
+v<-seq(5,20,0.5)
+vc<-cut(x=v,breaks=c(1:2),labels = T,include.lowest = T)
+
+##merge I
+?merge()
+mt<-merge(x=m1, y=m2,by=c("transect.id","seq"),all.x=T,no.dups=T)
+nrow(m1)+nrow(m2)
+
+##join
+library(dplyr)
+mj<-dplyr::right_join(x=m1,y=m2,by=c('transect.id'))
+nrow(m1)+nrow(m2)
